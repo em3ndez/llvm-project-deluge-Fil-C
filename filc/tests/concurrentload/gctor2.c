1a2be12cb7641e438ba9cf8646398b8f5e039068
Fil Pizlo <pizlo@mac.com>
2025-02-19 19:07:39 -0800

    Increase the delays in some tests to make them more meaningful.

--- filc/tests/concurrentload/gctor2.c
+++ filc/tests/concurrentload/gctor2.c
@@ -6,7 +6,7 @@ static void foo(void) __attribute__((constructor));
 static void foo(void)
 {
     zprintf("gctor2: gonna sleep a bit.\n");
-    usleep(100);
+    usleep(10000);
 }
 
 void bar2(void)
