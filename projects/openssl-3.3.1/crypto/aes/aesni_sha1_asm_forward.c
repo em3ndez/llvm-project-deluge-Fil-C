8f7f8cd72fa77c33cdd803fa4b0f7210233dce9e
Fil Pizlo <pizlo@mac.com>
2025-10-11 14:22:48 -0700

    Fix aesni_sha forwarders to do checks based on the sha block size, not the aes block size.

--- projects/openssl-3.3.1/crypto/aes/aesni_sha1_asm_forward.c
+++ projects/openssl-3.3.1/crypto/aes/aesni_sha1_asm_forward.c
@@ -14,27 +14,27 @@ void aesni_cbc_sha1_enc(const void *inp, void *out, size_t blocks,
                         const AES_KEY *key, unsigned char iv[16],
                         SHA_CTX *ctx, const void *in0)
 {
-    zcheck_readonly(inp, zchecked_mul(blocks, 16));
-    zcheck(out, zchecked_mul(blocks, 16));
+    zcheck_readonly(inp, zchecked_mul(blocks, 64));
+    zcheck(out, zchecked_mul(blocks, 64));
     zcheck_readonly(key, sizeof(AES_KEY));
     zcheck(iv, 16);
     zcheck(ctx, sizeof(SHA_CTX));
-    zcheck_readonly(in0, zchecked_mul(blocks, 16));
+    zcheck_readonly(in0, zchecked_mul(blocks, 64));
     zunsafe_call("aesni_cbc_sha1_enc", inp, out, blocks, key, iv, ctx, in0);
 }
 
-/* The stitched decrypt thing seems to be implemented by disabled. */
+/* The stitched decrypt thing seems to be implemented but disabled. */
 #if 0
 void aesni256_cbc_sha1_dec(const void *inp, void *out, size_t blocks,
                            const AES_KEY *key, unsigned char iv[16],
                            SHA_CTX *ctx, const void *in0)
 {
-    zcheck_readonly(inp, zchecked_mul(blocks, 16));
-    zcheck(out, zchecked_mul(blocks, 16));
+    zcheck_readonly(inp, zchecked_mul(blocks, 64));
+    zcheck(out, zchecked_mul(blocks, 64));
     zcheck_readonly(key, sizeof(AES_KEY));
     zcheck(iv, 16);
     zcheck(ctx, sizeof(SHA_CTX));
-    zcheck_readonly(in0, zchecked_mul(blocks, 16));
+    zcheck_readonly(in0, zchecked_mul(blocks, 64));
     zunsafe_call("aesni256_cbc_sha1_dec", inp, out, blocks, key, iv, ctx, in0);
 }
 #endif
--- projects/openssl-3.3.1/crypto/aes/aesni_sha256_asm_forward.c
+++ projects/openssl-3.3.1/crypto/aes/aesni_sha256_asm_forward.c
@@ -14,15 +14,15 @@ int aesni_cbc_sha256_enc(const void *inp, void *out, size_t blocks,
                          const AES_KEY *key, unsigned char iv[16],
                          SHA256_CTX *ctx, const void *in0)
 {
-    zcheck_readonly(inp, zchecked_mul(blocks, 16));
-    zcheck(out, zchecked_mul(blocks, 16));
+    zcheck_readonly(inp, zchecked_mul(blocks, 64));
+    zcheck(out, zchecked_mul(blocks, 64));
     if (key)
         zcheck_readonly(key, sizeof(AES_KEY));
     if (iv)
         zcheck(iv, 16);
     if (ctx)
         zcheck(ctx, sizeof(SHA_CTX));
-    zcheck_readonly(in0, zchecked_mul(blocks, 16));
+    zcheck_readonly(in0, zchecked_mul(blocks, 64));
     return zunsafe_call("aesni_cbc_sha256_enc", inp, out, blocks, key, iv, ctx, in0);
 }
 
