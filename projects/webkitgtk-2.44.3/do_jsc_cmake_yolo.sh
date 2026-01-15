74f6b8c007de2d3dc7cc4d57f5b621aa859b78c7
Saam Barati <saam@hey.com>
2026-01-14 22:37:18 -0800

    update cmake to build all of webkit

---
+++ projects/webkitgtk-2.44.3/do_jsc_cmake_yolo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+set -x
+
+mkdir build
+cd build
+cmake .. -DPORT=JSCOnly -DCMAKE_BUILD_TYPE=RelWithDebInfo -G Ninja -DENABLE_JIT=OFF -DENABLE_C_LOOP=ON -DENABLE_SAMPLING_PROFILER=OFF -DENABLE_WEBASSEMBLY=OFF -DUSE_SYSTEM_MALLOC=ON
+
+
+set +x
+
+echo "SUCCESS"
+echo "Now do (cd build && ninja) to build."
